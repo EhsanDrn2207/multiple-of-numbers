cb9030bc1f61317fa8de1b7c6e9187ed6b6335ca
Ehsan Doroudian <ehsan.dorodian@gmail.com>
2025-03-03 22:00:21 +0330

    implement the program

--- main.cpp
+++ main.cpp
@@ -3,7 +3,43 @@ using namespace std;
 
 int main(){
 
-	cout << "Hello World" << endl;
+	int x, y;
+	bool answer;
+
+	while(true)
+	{
+        cout << "Enter x number: ";
+        cin >> x;
+
+        if(x<=0)
+        {
+            cout << "The number must be positive. Please try again." << endl;
+            cout << "---------------------------------------------------" << endl;
+        }
+        else
+        {
+            break;
+        }
+
+	}
+
+	cout << "Enter y number: ";
+    cin >> y;
+
+	answer = (y%x == 0) ? 1 : 0;
+
+	if (answer==1)
+	{
+        cout << "The answer is True";
+
+	}
+
+	else
+	{
+        cout << "The answer is False";
+    }
+	cin.ignore();
+	cin.get();
 	return 0;
 
 }
